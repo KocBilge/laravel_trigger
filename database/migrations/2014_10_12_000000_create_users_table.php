5e2b4a891103e530eb870bd7bcca7e994218b037
Bilge Koç <93088437+KocBilge@users.noreply.github.com>
2023-12-01 00:55:31 +0300

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,1 +1,23 @@
+<?php
 
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+
+class CreateUserNotificationsTrigger extends Migration {
+    public function up() {
+        DB::unprepared('
+            CREATE TRIGGER after_user_insert
+            AFTER INSERT ON users
+            FOR EACH ROW
+            BEGIN
+                INSERT INTO user_notifications (user_id, message, created_at, updated_at)
+                VALUES (NEW.id, CONCAT("New user registered: ", NEW.username), NOW(), NOW());
+            END
+        ');
+    }
+
+    public function down() {
+        DB::unprepared('DROP TRIGGER IF EXISTS after_user_insert');
+    }
+}
